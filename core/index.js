18961e2beff50f0b935dbe1f0a556ba81f01f081
ZERRAI Sami <sami.zerrai@gmail.com>
2022-04-05 14:45:44 +0200

    add createElement

--- core/index.js
+++ core/index.js
@@ -1,4 +1,5 @@
 import Component from "./component.js";
+import { prop_access, type_check, type_check_v1 } from "./utils.js";
 
 export const MiniReact = {
   Component: Component,
@@ -21,15 +22,34 @@ export const MiniReact = {
     } else {
       const node = document.createElement(element);
 
-      if (childrens) {
-        for (let child of childrens) {
-          if (child === undefined) continue;
-          if (typeof child === "string") {
-            node.appendChild(document.createTextNode(child.interpolate(properties)));
-          } else {
-            node.appendChild(child);
-          }
-        }
+        if(childrens) {
+
+            if (!type_check_v1(childrens, 'array')) {
+
+                let child = childrens;
+                    console.log("child");
+                    console.log(child);
+                    if (typeof child === "string") {
+                        if (properties) node.appendChild(document.createTextNode(child.interpolate(properties)));
+                        else node.appendChild(document.createTextNode(child));
+
+                    } else {
+                        node.appendChild(child);
+                    }
+
+            }else {
+                childrens.forEach(child => {
+                    console.log("child");
+                    console.log(child);
+                    //   if (child === undefined) continue;
+
+                    if (typeof child === "string") {
+                        node.appendChild(document.createTextNode(child.interpolate(properties)));
+                    } else {
+                        node.appendChild(child);
+                    }
+                });
+            }
       }
 
       if (properties) {
